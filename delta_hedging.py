6453c397d9f9a25d76aad4ee362f70cd5ccdc414
romeucampos <romeu.campos@hotmail.com>
2020-11-14 20:44:05 -0300

    add round amount

--- delta_hedging.py
+++ delta_hedging.py
@@ -4,7 +4,7 @@ import logging
 from math import log
 
 from deribit_api import RestClient, requests
-
+# teste
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
@@ -33,7 +33,7 @@ class Hedge:
         res = requests.get(f'https://www.deribit.com/api/v2/public/ticker?instrument_name={symbol}').json()
         return res
 
-    def get_equity(self, currency='BTC'):
+    def get_equity(self, currency='BTC') -> str:
         account_summary = self.client.get_account_summary(currency)
         return account_summary['equity']
 
@@ -49,15 +49,21 @@ class Hedge:
         resp = log(ticker / trade)
         return resp, ticker, trade
 
+    def round_amount(self, delta_total) :
+        quaty = abs(round(self.get_ticker() * delta_total / 10) * 10)
+        if quaty < 10:
+            return 10
+        return quaty
+
     def delta_hedge(self, ln, delta_total):
         if  ln > self.gamma_theta:
-            quaty = round(self.get_ticker() * delta_total / 10) * 10
+            quaty = self.round_amount(delta_total)
             last_sell = self.client.sell(instrument=self.symbol, amount=quaty, price=0.00001, type='market')
             self.last_trade = last_sell['order']['average_price']
             logging.info(last_sell)
 
         elif ln < -self.gamma_theta:
-            quaty = (round(self.get_ticker() * delta_total / 10) * 10) * -1
+            quaty = (self.round_amount(delta_total))
             last_buy = self.client.buy(instrument=self.symbol, amount=quaty, price=0.00001, type='market')
             self.last_trade = last_buy['order']['average_price']
             logging.info(last_buy)    
@@ -68,13 +74,13 @@ class Hedge:
         self.info(delta_total, ln, ticker, trade)
 
         if  0 < delta_total:
-            quaty = round(self.get_ticker() * delta_total / 10) * 10
+            quaty = self.round_amount(delta_total)
             last_sell = self.client.sell(instrument=self.symbol, amount=quaty, price=0.00001, type='market')
             self.last_trade = last_sell['order']['average_price']
             logging.info(last_sell)
 
         elif 0 > delta_total:
-            quaty = (round(self.get_ticker() * delta_total / 10) * 10) * -1
+            quaty = self.round_amount(delta_total)
             last_buy = self.client.buy(instrument=self.symbol, amount=quaty, price=0.00001, type='market')
             self.last_trade = last_buy['order']['average_price']
             logging.info(last_buy)    
@@ -96,8 +102,8 @@ class Hedge:
         except requests.exceptions.ConnectionError as e:
             logging.info(e)
             time.sleep(TIME_LOOP)
-            self.run()
-            
+            self.run()    
+
 
 if __name__ == '__main__':
     hedge = Hedge()
